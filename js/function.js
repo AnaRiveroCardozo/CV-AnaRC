a2c84a0758c9f7f726abae97ba388a1019ab0bd1
Ana RC <anariverocardozo@gmail.com>
2023-02-28 08:13:19 -0300

    Primera escritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,8 @@
+function mostrar(){
+    document.getElementById("demo").innerHTML = "Trabajo de forma responsable y organizada, tratando de avanzar y especializarme en mi carrera, explorando distintas áreas, pero siempre sobre mis bases en seguridad e higiene en lo laboral.";
+}
+
+function displayDate() {
+    document.getElementById("demo2").innerHTML = Date();
+  }
+
